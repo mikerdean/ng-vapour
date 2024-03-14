41c0ebac8c6691f4c8001b64e632d218fa6b3c59
mikerdean <37405839+mikerdean@users.noreply.github.com>
2024-03-14 23:27:07 +0000

    Move to signals.

--- src/components/images/fontawesome-icon.component.ts
+++ src/components/images/fontawesome-icon.component.ts
@@ -1,9 +1,8 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Input,
-  type OnChanges,
-  type SimpleChanges,
+  computed,
+  input,
 } from "@angular/core";
 import {
   icon as parseIcon,
@@ -25,86 +24,86 @@ import type {
   selector: "fa-icon",
   templateUrl: "fontawesome-icon.component.svg",
 })
-export class FontawesomeIconComponent implements OnChanges {
-  @Input() animation?: FontAwesomeAnimation;
-  @Input() border = false;
-  @Input() class?: ClassOrStyle;
-  @Input() fixedWidth = false;
-  @Input() flip?: FlipProp;
-  @Input({ required: true }) icon!: IconDefinition;
-  @Input() inverse = false;
-  @Input() pull?: "left" | "right";
-  @Input() rotate?: FontAwesomeRotation;
-  @Input() size?: SizeProp;
-  @Input() style?: ClassOrStyle;
-  @Input() swapOpacity = false;
+export class FontawesomeIconComponent {
+  readonly animation = input<FontAwesomeAnimation>();
+  readonly border = input(false);
+  readonly class = input<ClassOrStyle>();
+  readonly fixedWidth = input(false);
+  readonly flip = input<FlipProp>();
+  readonly icon = input.required<IconDefinition>();
+  readonly inverse = input(false);
+  readonly pull = input<"left" | "right">();
+  readonly rotate = input<FontAwesomeRotation>();
+  readonly size = input<SizeProp>();
+  readonly style = input<ClassOrStyle>();
+  readonly swapOpacity = input(false);
 
-  parsedIcon!: Icon;
+  readonly classList = computed<ClassOrStyle>(() => {
+    const animation = this.animation();
+    const flip = this.flip();
+    const pull = this.pull();
+    const size = this.size();
 
-  ngOnChanges(changes: SimpleChanges): void {
-    const iconChange = changes["icon"];
-
-    if (iconChange) {
-      this.parsedIcon = parseIcon(iconChange.currentValue);
-    }
-  }
-
-  get classList(): ClassOrStyle {
     return {
       "svg-inline--fa": true,
-      "fa-border": this.border,
-      "fa-fw": this.fixedWidth,
-      "fa-inverse": this.inverse,
-      "fa-swap-opacity": this.swapOpacity,
+      "fa-border": this.border(),
+      "fa-fw": this.fixedWidth(),
+      "fa-inverse": this.inverse(),
+      "fa-swap-opacity": this.swapOpacity(),
 
-      "fa-beat": this.animation === "beat",
-      "fa-fade": this.animation === "fade",
-      "fa-beat-fade": this.animation === "beat-fade",
-      "fa-bounce": this.animation === "bounce",
-      "fa-flip": this.animation === "flip",
-      "fa-shake": this.animation === "shake",
-      "fa-spin": this.animation === "spin",
-      "fa-spin-pulse": this.animation === "spin-pulse",
+      "fa-beat": animation === "beat",
+      "fa-fade": animation === "fade",
+      "fa-beat-fade": animation === "beat-fade",
+      "fa-bounce": animation === "bounce",
+      "fa-flip": animation === "flip",
+      "fa-shake": animation === "shake",
+      "fa-spin": animation === "spin",
+      "fa-spin-pulse": animation === "spin-pulse",
 
-      "fa-flip-horizontal": this.flip === "horizontal",
-      "fa-flip-vertical": this.flip === "vertical",
-      "fa-flip-both": this.flip === "both",
+      "fa-flip-horizontal": flip === "horizontal",
+      "fa-flip-vertical": flip === "vertical",
+      "fa-flip-both": flip === "both",
 
-      "fa-pull-left": this.pull === "left",
-      "fa-pull-right": this.pull === "right",
+      "fa-pull-left": pull === "left",
+      "fa-pull-right": pull === "right",
 
-      "fa-rotate-by": this.rotate !== undefined,
+      "fa-rotate-by": this.rotate() !== undefined,
 
-      "fa-2xs": this.size === "2xs",
-      "fa-xs": this.size === "xs",
-      "fa-sm": this.size === "sm",
-      "fa-lg": this.size === "lg",
-      "fa-xl": this.size === "xl",
-      "fa-2xl": this.size === "2xl",
-      "fa-1x": this.size === "1x",
-      "fa-2x": this.size === "2x",
-      "fa-3x": this.size === "3x",
-      "fa-4x": this.size === "4x",
-      "fa-5x": this.size === "5x",
-      "fa-6x": this.size === "6x",
-      "fa-7x": this.size === "7x",
-      "fa-8x": this.size === "8x",
-      "fa-9x": this.size === "9x",
-      "fa-10x": this.size === "10x",
+      "fa-2xs": size === "2xs",
+      "fa-xs": size === "xs",
+      "fa-sm": size === "sm",
+      "fa-lg": size === "lg",
+      "fa-xl": size === "xl",
+      "fa-2xl": size === "2xl",
+      "fa-1x": size === "1x",
+      "fa-2x": size === "2x",
+      "fa-3x": size === "3x",
+      "fa-4x": size === "4x",
+      "fa-5x": size === "5x",
+      "fa-6x": size === "6x",
+      "fa-7x": size === "7x",
+      "fa-8x": size === "8x",
+      "fa-9x": size === "9x",
+      "fa-10x": size === "10x",
 
-      ...this.class,
+      ...this.class(),
     };
-  }
+  });
 
-  get styleList() {
+  readonly parsedIcon = computed<Icon>(() => parseIcon(this.icon()));
+
+  readonly styleList = computed(() => {
+    const rotation = this.rotate();
+
     return {
-      "--fa-rotate-angle": this.rotate ? `${this.rotate}deg` : undefined,
-      ...this.style,
+      "--fa-rotate-angle": rotation ? `${rotation}deg` : undefined,
+      ...this.style(),
     };
-  }
+  });
 
-  get viewBox() {
-    const [width, height] = this.parsedIcon.icon;
+  readonly viewBox = computed(() => {
+    const parsed = this.parsedIcon();
+    const [width, height] = parsed.icon;
     return `0 0 ${width} ${height}`;
-  }
+  });
 }
