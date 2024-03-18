5f4fac5982729f6a20de8babc639a5754557c4d9
mikerdean <37405839+mikerdean@users.noreply.github.com>
2024-03-18 22:18:35 +0000

    Translate 404 page.

--- src/components/views/error/error404.component.ts
+++ src/components/views/error/error404.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 import { AlertComponent } from "@vapour/components/core/alert.component";
 import { MainContentComponent } from "@vapour/components/root/main-content.component";
+import { TranslatePipe } from "@vapour/pipes/translate";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AlertComponent, MainContentComponent],
+  imports: [AlertComponent, MainContentComponent, TranslatePipe],
   standalone: true,
   selector: "error404",
   templateUrl: "error404.component.html",
