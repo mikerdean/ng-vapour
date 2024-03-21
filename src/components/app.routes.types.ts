71ae3a3b241276c7ae65e0cc6100f51d0f3c9d27
mikerdean <37405839+mikerdean@users.noreply.github.com>
2024-03-21 20:29:04 +0000

    Remove unused file.

--- src/components/app.routes.types.ts
+++
@@ -1,6 +0,0 @@
-import type { Route } from "@angular/router";
-import type { IconDefinition } from "@fortawesome/fontawesome-common-types";
-
-export type RouteWithMetadata = Route & {
-  icon?: IconDefinition;
-};
