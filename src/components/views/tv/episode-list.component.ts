4ba90ab1825d68c93bb4a6e0a36e575e21735beb
mikerdean <37405839+mikerdean@users.noreply.github.com>
2024-03-28 23:47:45 +0000

    Add thumbnails to episode list.

--- src/components/views/tv/episode-list.component.ts
+++ src/components/views/tv/episode-list.component.ts
@@ -2,13 +2,19 @@ import { ChangeDetectionStrategy, Component, input } from "@angular/core";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
 import { FontawesomeIconComponent } from "@vapour/components/images/fontawesome-icon.component";
+import { ThumbnailComponent } from "@vapour/components/images/thumbnail.component";
 import { DurationPipe } from "@vapour/pipes/duration";
 import { PadStartPipe } from "@vapour/pipes/padStart";
 import type { VideoDetailsEpisode } from "@vapour/shared/kodi";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [DurationPipe, FontawesomeIconComponent, PadStartPipe],
+  imports: [
+    DurationPipe,
+    FontawesomeIconComponent,
+    PadStartPipe,
+    ThumbnailComponent,
+  ],
   selector: "episode-list",
   standalone: true,
   templateUrl: "episode-list.component.html",
