d4652078978e8ca755f3058b4dcb8608432bf095
mikerdean <37405839+mikerdean@users.noreply.github.com>
2026-03-06 19:52:34 +0000

    refactor: remember to remove event listeners

--- src/signals/mediaQuery.ts
+++ src/signals/mediaQuery.ts
@@ -1,11 +1,19 @@
-import { signal, type Signal } from "@angular/core";
+import { effect, signal, type Signal } from "@angular/core";
 
 export function mediaQuery(query: string): Signal<boolean> {
   const matchMedia = window.matchMedia(query);
   const mediaQuerySignal = signal(matchMedia.matches);
 
-  matchMedia.addEventListener("change", (ev) => {
+  const listener = (ev: MediaQueryListEvent): void => {
     mediaQuerySignal.set(ev.matches);
+  };
+
+  matchMedia.addEventListener("change", listener);
+
+  effect((onCleanup) => {
+    onCleanup(() => {
+      matchMedia.removeEventListener("change", listener);
+    });
   });
 
   return mediaQuerySignal.asReadonly();
