6d86b4bae95cde1f083b1a1b32258b51d20c0b30
mikerdean <37405839+mikerdean@users.noreply.github.com>
2026-03-12 18:20:15 +0000

    feat: add translateOne method

--- src/signals/translate.ts
+++ src/signals/translate.ts
@@ -17,31 +17,37 @@ type TranslationOutput<T> = {
 export function translate<T extends TranslationInput>(
   translations: T,
 ): TranslationOutput<T> {
-  assertInInjectionContext(translate);
-
-  const translationState = inject(TranslationState);
-
   const output: Record<string, Signal<string>> = {};
 
   for (const [key, translation] of Object.entries(translations)) {
-    const signal = computed(() => {
-      const tkey =
-        typeof translation === "string" ? translation : translation[0];
+    const isString = typeof translation === "string";
+    const translationKey = isString ? translation : translation[0];
+    const options = isString ? undefined : translation[1];
 
-      const options =
-        typeof translation === "string" ? undefined : translation[1]();
-
-      const t = translationState.t();
-
-      if (!t) {
-        return "__translation_failed__";
-      }
-
-      return t(tkey, options);
-    });
-
-    output[key] = signal;
+    output[key] = translateOne(translationKey, options);
   }
 
   return output as TranslationOutput<T>;
+}
+
+export function translateOne(
+  key: ParseKeys,
+  options?: () => TOptions,
+): Signal<string> {
+  assertInInjectionContext(translateOne);
+
+  const translationState = inject(TranslationState);
+
+  const signal = computed(() => {
+    const t = translationState.t();
+    const unwrappedOptions = options ? options() : undefined;
+
+    if (!t) {
+      return "__translation_failed__";
+    }
+
+    return t(key, unwrappedOptions);
+  });
+
+  return signal;
 }
